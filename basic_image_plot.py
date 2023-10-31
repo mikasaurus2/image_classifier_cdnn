ef4b74104dea70a302d4a71c2b8affd53b71d232
mikasaurus2 <>
2023-10-30 21:41:54 -0700

    Formatting with Black

--- basic_image_plot.py
+++ basic_image_plot.py
@@ -3,15 +3,15 @@ from matplotlib import pyplot
 from matplotlib.image import imread
 
 # define location of dataset
-folder = 'dogs-vs-cats/train/'
+folder = "dogs-vs-cats/train/"
 
 # plot first few images
 for i in range(9):
     # define subplot
     pyplot.subplot(330 + 1 + i)
     # define filename
-    #filename = folder + 'dog.' + str(i) + '.jpg'
-    filename = folder + 'cat.' + str(i) + '.jpg'
+    # filename = folder + 'dog.' + str(i) + '.jpg'
+    filename = folder + "cat." + str(i) + ".jpg"
     # load image pixels
     image = imread(filename)
     # plot raw pixel data
--- setup_data.py
+++ setup_data.py
@@ -8,12 +8,13 @@ def create_directories(dataset_home: str) -> None:
     # Split the training data into directory structure optimized
     # for Keras.
     print("Creating directories...")
-    subdirs = ['train/', 'test/']
+    subdirs = ["train/", "test/"]
     for subdir in subdirs:
-        labeldirs = ['dogs/', 'cats/']
+        labeldirs = ["dogs/", "cats/"]
         for labeldir in labeldirs:
             newdir = dataset_home + subdir + labeldir
             os.makedirs(newdir, exist_ok=True)
+
 
 def copy_images(dataset_home: str) -> None:
     # Copy images to the new training directory.
@@ -23,34 +24,36 @@ def copy_images(dataset_home: str) -> None:
     print("Copying images...")
     random.seed(1)
     test_ratio = 0.25
-    src_directory = 'dogs-vs-cats/train/'
+    src_directory = "dogs-vs-cats/train/"
     for file in os.listdir(src_directory):
-        src_image = src_directory + '/' + file
-        dst_dir = 'test/' if random.random() < test_ratio else 'train/'
-        if file.startswith('cat'):
-            dst_image = dataset_home + dst_dir + 'cats/' + file
+        src_image = src_directory + "/" + file
+        dst_dir = "test/" if random.random() < test_ratio else "train/"
+        if file.startswith("cat"):
+            dst_image = dataset_home + dst_dir + "cats/" + file
             shutil.copyfile(src_image, dst_image)
-        elif file.startswith('dog'):
-            dst_image = dataset_home + dst_dir + 'dogs/' + file
+        elif file.startswith("dog"):
+            dst_image = dataset_home + dst_dir + "dogs/" + file
             shutil.copyfile(src_image, dst_image)
 
     total_initial_images = len(os.listdir(src_directory))
-    cat_train_count = len(os.listdir(dataset_home + 'train/cats/'))
-    cat_test_count = len(os.listdir(dataset_home + 'test/cats/'))
-    dog_train_count = len(os.listdir(dataset_home + 'train/dogs/'))
-    dog_test_count = len(os.listdir(dataset_home + 'test/dogs/'))
-    print(f'{total_initial_images} total images')
-    print(f'{cat_train_count} cat train images')
-    print(f'{cat_test_count} cat test images')
-    print(f'{dog_train_count} dog train images')
-    print(f'{dog_test_count} dog test images')
+    cat_train_count = len(os.listdir(dataset_home + "train/cats/"))
+    cat_test_count = len(os.listdir(dataset_home + "test/cats/"))
+    dog_train_count = len(os.listdir(dataset_home + "train/dogs/"))
+    dog_test_count = len(os.listdir(dataset_home + "test/dogs/"))
+    print(f"{total_initial_images} total images")
+    print(f"{cat_train_count} cat train images")
+    print(f"{cat_test_count} cat test images")
+    print(f"{dog_train_count} dog train images")
+    print(f"{dog_test_count} dog test images")
+
 
 def main() -> int:
     print("Preparing data...")
-    dataset_home = 'dataset_dogs_vs_cats/'
+    dataset_home = "dataset_dogs_vs_cats/"
     create_directories(dataset_home)
     copy_images(dataset_home)
     return 0
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sys.exit(main())
